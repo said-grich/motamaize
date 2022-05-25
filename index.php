8634ec21ad249910071444866ce2e50d0353d452
said grich <said.grich@edu.uca.ma>
2022-05-25 11:29:54 +0100

    first commit by grich

---
+++ index.php
@@ -0,0 +1,260 @@
+<?php
+$data = [];
+$data['title_page'] = "محطات التعلم";
+?>
+<!DOCTYPE HTML>
+<html dir="rtl">
+
+<head>
+	<title><?php echo $data['title_page'] ?></title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link href="https://fonts.googleapis.com/css?family=Tajawal:200,300,400,500,700,800,900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<noscript>
+		<link rel="stylesheet" href="assets/css/noscript.css" />
+	</noscript>
+</head>
+
+<body class="is-preload">
+	<!-- Header -->
+	<section id="header">
+		<div class="inner">
+			<span class="icon solid major fa-book-open"></span>
+			<h1>محطات التعلم المتمايز الثابتة والمتغيرة</h1>
+			<p>ٌيقدم المعلم ملخصا للتجربة تتضح فيه أهمية التجربة وفكرتها ومسوغ اختيار سواء أكانت تجربة أو تطوير تجربة قائمة </p>
+			<ul class="actions special">
+				<li style="display: contents;"><a href="#one" class="button scrolly">اكتشف</a></li>
+			</ul>
+		</div>
+	</section>
+
+	<!-- One -->
+	<section id="one" class="main style1">
+		<div class="container">
+			<div class="row gtr-150">
+				<div class="col-6 col-12-medium">
+					<header class="major">
+						<h2>أهمية التجربة :</h2>
+					</header>
+					<ul>
+						<li>رفع مستوى التحصيل الدراسي لجميع الطالب.</li>
+						<li>توفير الاحتياجات التعلبمية لجميع الطالب المتمايزينن</li>
+						<li>إتاحة قدر أكبر من الاستقلالية والحرية في التعلم للطالب</li>
+						<li>زيادة ثقة التلاميذ بأنفسهم وقدرتهم على التعلم</li>
+						<li>تعديل الغهم الخاطئ للمفاهيم العلمية لدى طلاب</li>
+						<li>التغلب على مشكلة نقص الادوات و الموارد حيث أن توظيف هذه الاستراتيجية يتطلب أن يتم توفير الادوات لمجموعة واحدة فقط بدلا من توفيريها لجميع المجموعات</li>
+						<li>انخراط الطلاب في عمليات العلم من خلال اشراكهم في تنفيذ التجارب المعملية بدلا عن الاكتفاء بمشاهدة المعلم و هو ينفذها</li>
+						<li>كسر الجمود و الملل الصفي و اعطاء الفرصة للطلاب للحركة داخل الفصل</li>
+						<li>تنويع الخبرات العلمية و النظرية التي يمر بها الطلاب داخل الصف و بالتالي تلبية احتياجاتهم التعلمية</li>
+					</ul>
+				</div>
+				<div class="col-6 col-12-medium imp-medium">
+					<span class="image fit">
+						<span class="image fit"><img src="assets/images/2.jpeg" alt="" /></span>
+
+					</span>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="two" class="main style2">
+		<div class="container">
+
+			<div class="row gtr-150">
+				<div class="col-6 col-12-medium">
+					<header class="major">
+						<h2>فكرة المشروع</h2>
+					</header>
+					<h3>محطات التعلم المتمايز و المتغيرة تقدم بيئة تعليمية مناسبة لجميع الطلبة سيما أن الطلبة يختلفون عن بعضهم من نواحي متعددة:</h3>
+					<ul>
+						<li>اختلافات في البيئة المنزلية</li>
+						<li>اختلاف في الثقافة </li>
+						<li>في الخبرات</li>
+						<li>في الاستجابة لمتطلبات الدراسة </li>
+						<li>في طرق ادراك العالم</li>
+					</ul>
+
+
+				</div>
+				<div class="col-6 col-12-medium">
+					<p>
+						ويقصد بمحطات التعلم الثابتة و المتغيرة هي تقسيم الفصل الدراسي الى مواقع فعلية )محطات( التي
+						عادة ما تكون مجموعة من الطاوالت المجهزة بأنشطة متنوعة؛ حيث يعمل الطلاب في مجموعات كل
+						مجموعة لها نمط تعلم مختلف عن الاخرى وكل مجموعة تتجول في مسار مخصص من خمس
+						محطات المحطة الاولي تتشارك فيها مع جميع المسارات ذات أنماط التعلم المختلفة تسمى )المحطة
+						الثابتة الاولى( وهي عبارة عن محطة يقوم المعلم بشرح المفاهيم الجديدة وعرض المعلومات المتعلمة
+						بالدرس لجميع المسارات باستخدام عروض البورتبونت واستخدام السبورة الذكية لمدة 8 دقائق ثم
+						يفترقون الطلاب كل مجموعة حسب نمط تعلمها في مسارات مختلفة مسار لنمط التعلم الحركي ومسار
+						لنمط التعلم البصري ومسار لنمط التعلم السمعي وكل مسار من ثلاث محطات مغيرة. بحيث تعمل كل
+						مجموعة نمط في مهمة مختلفة حيت يطلب من الطالب حل مشكلة والاجابة على بعض الاسئلة باستخدام
+						المواد المتوفرة، و يسمح للطالب بالعمل بشكل فردي في نفس المجموعة أو جماعي مع ما يصل إلى) 5-
+						6 )طلاب أخرون يربطهم نفس نمط التعلم
+
+					</p>
+				</div>
+			</div>
+	</section>
+
+	<section id="one" class="main style1">
+		<div class="container">
+			<div class="row gtr-150">
+				<div class="col-6 col-12-medium imp-medium">
+					<span class="image fit">
+						<span class="image fit"><img src="assets/images/3.jpeg" alt="" /></span>
+
+					</span>
+				</div>
+
+				<div class="col-6 col-12-medium">
+					<header class="major">
+						<h2>نشاط المحطة</h2>
+					</header>
+					<p>تنتقل مجموعة الطلاب المتشابهين في نمط التعلم من محطة إلى أخرى في مسار مخصص لقراءة
+						مجموعة من المهام تتمحور حول حدث أو موضوع أو حل مشكلة والاجابة على بعض األسئلة باستخدام
+						المواد المتوفرة ،من وجهات نظر متعددة ومشاهدتها و تفسيرها ، ويسمح للطالب بالعمل بشكل فردي في
+						نفس المجموعة أو جماعي مع ما يصل إلى 5 -6 طلاب أخرون يربطهم نفس نمط التعلم. وتمضي
+						مجموعة الطلاب وقتا مخصصا في كل محطة للتفاعل مع المادة تستمر كل محطة 11 دقائق ثم ينتقل
+						طلاب المجموعة الواحدة الى المحطة الاخرى وبعد انتهاء كل مسار من ثلاث محطات مخصصه له
+						تتجمع المسارات مرة أخرى في محطة ثابتة ثانية وهي الاخيرة لجميع المسارات تسمى )المحطة الثابتة
+						الثانية( مدتها )7 )دقائق مخصصة لتقويم الاداء تستخدم فيها أدوات مختلفة من التقويم.</p>
+				</div>
+
+			</div>
+		</div>
+	</section>
+
+	<!-- Two -->
+	<section id="two" class="main style2">
+		<div class="container">
+			<div class="row gtr-150">
+				<div class="col-6 col-12-medium">
+					<ul class="major-icons">
+
+						<li><span class="icon solid style3 major fa-camera-retro"></span></li>
+						<li><span class="icon solid style4 major fa-cog"></span></li>
+						<li><span class="icon solid style5 major fa-desktop"></span></li>
+						<li><span class="icon solid style6 major fa-calendar"></span></li>
+					</ul>
+				</div>
+				<div class="col-6 col-12-medium">
+					<header class="major">
+						<h2>مسوغ التجربة :</h2>
+					</header>
+					<h3>أن الطلاب يختلفون عن بعضهم البعض. الطلاب من نفس العمر ليسوا متشابهين في التعليم العادي يقدم المعلم مثيرا واحدا
+						أو هدفا واجد , و يكلف الطلبة بنشاط واحد ليحققو نفس المخرجات مع انهم مختلفون في الفروق الفردية.
+					</h3>
+					<ul>
+						<li>أما إذا أراد المعلم أن يراعي  الفروق الفردية فإنه يعمل على تقديم نفس المثابرة للجميع ونفس المهمة ولكن يقبل  منهم مخرجات مختلفة. ففي  هذه الحالة يراعي قدرات وإمكانات الطلبة فهم لا 
+ يستطيعون الوصول الى نفس النتائج أو المخرجات لأنهم متفاوتون في جميع قدراتهم.</li>
+						<li>أما إذا أراد المعلم تقديم تعليم متمايز فإنه يقدم نفس المثير ومهام متنوعة  ليصل الى نفس المخرجات.</li>
+					</ul>
+
+
+
+				</div>
+				<ul class="actions special">
+					<li><h3>لذلك محطات التعلم المتاميز استجابة منطقية لمواجهة تباين و تمايز الطلاب في الفصل الواحد، ليناسب اختالفهم، ومحاولة تقديم حل لرفع المستوى التعليمي بشكل عام لجميع فئات الطلاب دون تميز.</h3></li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<!-- One -->
+	<section id="one" class="main style1">
+		<div class="container">
+			<div class="row gtr-150">
+				<div class="col-6 col-12-medium">
+					<header class="major">
+						<h1>الاستاذ منصور مفرح المقاطي</h1>
+					</header>
+					<ul>
+						<li>
+							حاصل على جائزة جدة للمعلم المتميز عام 1436
+						</li>
+						<li>
+							المشاركة في الملتقى العلمي السادسبجامعة الملك عبدالعزيز
+						</li>
+						<li>
+							المشاركة في ملتقى العلوم
+						</li>
+						<li>
+							المشاركة في منتدى الجودة
+						</li>
+						<li>
+							المشاركة في ملتقى منسقي مشروع التعلم النشط بمدارسمكتب
+							تعليم وسط جدة
+						</li>
+						<li>
+							المشاركة في ملتقى قسم العلوم الثالث عشر
+						</li>
+						<li>
+							المشاركة في ملتقى العلمي األول إلدارة اإلشراف التربوي ومكاتب
+							التعليم بجدة
+						</li>
+						<li>
+							المشاركة في ملتقى جائزة جدة للملعم المتميز
+						</li>
+					</ul>
+
+				</div>
+				<div class="col-6 col-12-medium imp-medium">
+					<span class="image fit">
+						<span class="image fit"><img height="380px" src="assets/images/prof.png" alt="" /></span>
+					</span>
+				</div>
+				<ul class="actions special">
+					<li><a href="assets/pdf/cv.pdf" class="button wide primary">السيرة الذاتية </a></li>
+				</ul>
+			</div>
+		</div>
+	</section>
+
+	<!-- Three -->
+	<section id="three" class="main style1 special">
+		<div class="container">
+			<header class="major">
+				<h2>فيديوهات توضيحية لتجربة</h2>
+			</header>
+			<div class="row gtr-150">
+				<div class="col-4 col-12-medium">
+					<span class="image fit"><iframe width="280" height="280" src="https://www.youtube.com/embed/yjezmCGFhLs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></span>
+				</div>
+				<div class="col-4 col-12-medium">
+					<span class="image fit"><iframe width="280" height="280" src="https://www.youtube.com/embed/iHve47gE-Ss" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></span>
+				</div>
+				<div class="col-4 col-12-medium">
+					<span class="image fit"><iframe width="280" height="280" src="https://www.youtube.com/embed/XGYpMJIngTc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></span>
+				</div>
+				<div class="col-4 col-12-medium">
+					<span class="image fit"><iframe width="280" height="280" src="https://www.youtube.com/embed/Nn5rrCMNcD0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></span>
+				</div>
+
+			</div>
+		</div>
+	</section>
+
+
+
+	<!-- Footer -->
+	<section id="footer">
+		<ul class="icons">
+			<li><a href="#" class="icon brands alt fa-twitter"><span class="label">Twitter</span></a></li>
+			<li><a href="#" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+			<li><a href="#" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
+			<li><a href="#" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+			<li><a href="#" class="icon solid alt fa-envelope"><span class="label">Email</span></a></li>
+		</ul>
+
+	</section>
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.scrolly.min.js"></script>
+	<script src="assets/js/browser.min.js"></script>
+	<script src="assets/js/breakpoints.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
